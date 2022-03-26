343f2852a58781288fd272568839e16ff8fa3451
zeel-codder <zeelprajapati321@gmail.com>
2022-03-26 13:04:30 +0530

    add request log

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-// const cookieParser = require('cookie-parser');
-// app.use(cookieParser());
 
 dotenv.config({ path: "./.env" });
 require("./db/conn");
+
 
 const app = express();
 const authRouter = require("./router/auth");
 const userRouter = require("./router/user");
 
+const logRequestStart = (req, res, next) => {
+  res.on('finish', () => {
+      console.info(`${req.url} ${req.method} ${res.statusCode} ${res.statusMessage};`)
+  })
+  next()
+}
+
+
 app.use(cors());
 app.use(express.json());
+app.use(logRequestStart)
+
+
+
 
 // we link the router files to make our route easy
 app.use("/api/v1/auth", authRouter);
