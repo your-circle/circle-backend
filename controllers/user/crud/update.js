e963327138c1ec1646c6ecb2e9da0eee02c30cd6
Mitali-laroia <04042001mitali@gmail.com>
2022-05-25 21:11:59 +0530

    route definition for password rest

--- controllers/user/crud/update.js
+++ controllers/user/crud/update.js
@@ -1,4 +1,5 @@
 const { UserModel } = require("../../../db/models/user");
+const VerifyAuthToken = require("../../auth/auth");
 
 const {
   SuccessResponseHandler,
@@ -8,6 +9,8 @@ const {
 const {
   UserUpdateMessage,
 } = require("../../../utils/const/message");
+const { TokenExpiredError } = require("jsonwebtoken");
+const sendEmail = require("../../../utils/sendEmail");
 
 
 const UpdateUser = async (req, res) => {
@@ -25,5 +28,37 @@ const UpdateUser = async (req, res) => {
   }
 };
 
+const forgetpassword = async (req,res) => {
+  try {
+    const id = req.userID;
+    console.log(req.body.email);
+    const user = await UserModel.findOne({email: req.body.email});
+    if(!user) return res.status(400).send("user with given email does not exist");
+      const token = await user.generateAuthToken();
+      const link = `${process.env.BASE_URL}/password-reset/${id}/${token}`;
+      await sendEmail(user.email,"Password reset for your circle account", link);
+
+      res.send("password reset link sent to your email account");
+  } catch (error) {
+    res.send("An error occured");
+    return error;
+  }
+}
+
+const passwordReset = async (req,res) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    if(!user) return res.status(400).send("invalid link or experied");
+    const token = await user.generateAuthToken();
+    user.password = req.body.password;
+    await user.save();
+    res.send("password reset successfully");
+  } catch (error) {
+    res.send("An error occured");
+    console.log(error);
+  }
+}
 
 exports.UpdateUser = UpdateUser;
+exports.passwordReset = passwordReset;
+exports.forgetpassword = forgetpassword;
